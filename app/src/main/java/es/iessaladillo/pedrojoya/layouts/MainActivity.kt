6b756592a8ff1bfafa1698a697c33388b0b1570b
Salvador <salvadorfoncubierta.saladillo.gmail.com>
2020-10-29 19:27:05 +0100

    Finalizado

--- app/src/main/java/es/iessaladillo/pedrojoya/layouts/MainActivity.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/layouts/MainActivity.kt
@@ -2,12 +2,29 @@ package es.iessaladillo.pedrojoya.layouts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import es.iessaladillo.pedrojoya.layouts.databinding.ConstraintlayoutActivityBinding
+import es.iessaladillo.pedrojoya.layouts.databinding.LinearlayoutActivityBinding
+import es.iessaladillo.pedrojoya.layouts.databinding.RelativelayoutActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var bindingLinearLayout: LinearlayoutActivityBinding
+    private lateinit var bindingRelativeLayout: RelativelayoutActivityBinding
+    private lateinit var bindingConstraintLayout: ConstraintlayoutActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // TODO: USAR UNO DE LOS LAYOUTS DEFINIDOS PARA PROBARLO.
-        setContentView(R.layout.linearlayout_activity)
+        //Hecho con Linear Layout
+        //bindingLinearLayout = LinearlayoutActivityBinding.inflate(LayoutInflater.from(this))
+        //setContentView(bindingLinearLayout.root)
+
+        //Hecho con Relative Layout
+        //bindingRelativeLayout = RelativelayoutActivityBinding.inflate(LayoutInflater.from(this))
+        //setContentView(bindingRelativeLayout.root)
+
+        //Hecho con Constraint Layout
+        bindingConstraintLayout = ConstraintlayoutActivityBinding.inflate(LayoutInflater.from(this))
+        setContentView(bindingConstraintLayout.root)
     }
 }
